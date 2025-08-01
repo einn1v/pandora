855289d1eda3b63dbc94fb5b53cc275182111c1e
v <221338086+einn1v@users.noreply.github.com>
2025-08-02 01:21:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,5 @@
 import os
-import shutil
 from setuptools import setup
-from setuptools.command.install import install
-
-class Install(install):
-    def run(self):
-        install.run(self)
-
-        path = os.path.expanduser("~/.local/share")
-        directory = os.path.join(path, "Pandora")
-        
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        
-        src = os.path.join(os.path.dirname(__file__), "main.py")
-        dst = os.path.join(directory, "main.py")
-
-        if not os.path.exists(dst):
-            shutil.copy(src, dst)
-            print("Pandora installed successfully.")
-        else:
-            print("Pandora is already installed.")
 
 with open(os.path.dirname(__file__), "requirements.txt") as file:
     reqs = file.read().splitlines()
@@ -31,11 +10,6 @@ setup(
     version="1.0.0",
     packages=[],
     install_requires=reqs,
-    cmdclass={
-        "install": Install,
-        "init": Install,
-        "-i": Install,
-    },
     entry_points={
         "console_scripts": [
             "pandora = main:main",
