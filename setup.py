32df2de953a0f0169e0655d1c69260f735b53c27
v <221338086+einn1v@users.noreply.github.com>
2025-08-02 01:01:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,45 @@
 import os
-import time
+import shutil
+from setuptools import setup
+from setuptools.command.install import install
 
-print("You are currently installing PanDoRa. You will first need to install the required dependencies. Do you want to continue?\n[y/n]")
-if input().lower() == "y":
-    os.system("pip install -r requirements.txt")
-    print("Dependencies installed successfully.")
-    input()
-else:
-    print("Installation cancelled.")
-    time.sleep(1)
-    exit(0)
+class Install(install):
+    def run(self):
+        install.run(self)
+
+        path = os.path.expanduser("~/.local/share")
+        directory = os.path.join(path, "Pandora")
+        
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        
+        src = os.path.join(os.path.dirname(__file__), "main.py")
+        dst = os.path.join(directory, "main.py")
+
+        if not os.path.exists(dst):
+            shutil.copy(src, dst)
+            print("Pandora installed successfully.")
+        else:
+            print("Pandora is already installed.")
+
+with open(os.path.dirname(__file__), "requirements.txt") as file:
+    reqs = file.read().splitlines()
+
+setup(
+    name="pandora",
+    description="Pandora is a CLI password manager for safe and efficient password management.",
+    version="1.0.0",
+    packages=[],
+    install_requires=reqs,
+    cmdclass={
+        "install": Install,
+        "init": Install,
+        "-i": Install,
+    },
+    entry_points={
+        "console_scripts": [
+            "pandora = main:main",
+            "pdr = main:main",
+        ]
+    }
+)
